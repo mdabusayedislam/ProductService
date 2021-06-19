c7ac4f335bfb702b12cf98bab831b1ce892ace5b
Md. Abu Sayed Islam <abusayediit@outlook.com>
2021-06-19 18:49:04 +0600

    Add project files.

---
+++ ProductService/Repository/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using ProductService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductService.Repository
+{
+    public interface IProductRepository
+    {
+        void DeleteProduct(int productId);
+        Product GetProductByID(int productId);
+        IEnumerable<Product> GetProducts();
+        void InsertProduct(Product product);
+        void Save();
+        void UpdateProduct(Product product);
+    }
+}
